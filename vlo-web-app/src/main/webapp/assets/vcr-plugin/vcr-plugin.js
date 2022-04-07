bd14170eb533a400dae948377c761263cabf0cf2
Twan Goosen <twan@clarin.eu>
2022-04-07 17:58:10 +0300

    removing items from queue, preventing duplicates

--- vlo-web-app/src/main/webapp/assets/vcr-plugin/vcr-plugin.js
+++ vlo-web-app/src/main/webapp/assets/vcr-plugin/vcr-plugin.js
@@ -28,88 +28,128 @@ const queueViewTemplate = `
 `;
 
 class VCRIntegration {
-    
+
     constructor(config) {
         this.config = config;
     }
-    
+
     getQueue() {
         const queue = window.localStorage.getItem('vcrQueue');
-        if(queue) {
+        if (queue) {
             return JSON.parse(queue);//['content'];
         } else {
             return this.saveQueue([]);
         }
     }
-    
+
     saveQueue(queue) {
-        window.localStorage.setItem('vcrQueue', JSON.stringify(queue))  ;//{'content': queue}));
+        window.localStorage.setItem('vcrQueue', JSON.stringify(queue));//{'content': queue}));
         return queue;
     }
-    
+
     clearQueue() {
         window.localStorage.removeItem('vcrQueue');
     }
-    
-    addToQueue(url) {
+
+    getItemIndex(url) {
         const queue = this.getQueue();
-        queue.push({'url': url});
-        this.saveQueue(queue);
-        console.log('Queue: ');
-        console.dir(this.getQueue())
-        this.renderQueue();
+        for (let i = 0; i < queue.length; i++) {
+            if (queue[i]['url'] === url) {
+                return i;
+            }
+        }
+        return -1;
     }
-    
+
+    removeFromQueue(url) {
+        console.log('Remove from queue: ' + url);
+        const index = this.getItemIndex(url);
+        if (index >= 0) {
+            console.log('Remove item ' + index);
+
+            const queue = this.getQueue();
+            queue.splice(index, 1);
+
+            this.saveQueue(queue);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    addToQueue(url, title) {
+        if(this.getItemIndex(url) >= 0) {
+            console.log('URL already in queue');
+        } else {
+            const queue = this.getQueue();
+            queue.push({'url': url, 'title': title});
+            this.saveQueue(queue);
+            console.log('Queue: ');
+            console.dir(this.getQueue());
+            this.renderQueue();
+        }
+    }
+
     renderQueue() {
-        if($("body #vcrQueue").length) {
+        if ($("body #vcrQueue").length) {
             $("body #vcrQueue").remove();
         }
         const queue = this.getQueue();
-        if(queue && queue.length > 0) {
+        if (queue && queue.length > 0) {
             $("body").append(queueViewTemplate);
-            const list = $("#vcrQueue ul");        
-            queue.forEach(function(qi){
-                list.append('<li>'+qi['url']+'</li>');
+            const list = $("#vcrQueue ul");
+            queue.forEach(function (qi) {
+                list.append('<li data-vcr-url="' + qi['url'] + '">' + qi['title'] + ' <a class="removeFromVcrQueue" style="color: red; text-decoration: none;" href="#">X</a></li>');
             });
         }
     }
 }
 
 class VCRIntegrationEventHandler {
-    
+
     handleClearQueueEvent() {
         console.log('clear queue');
         vcrIntegration.clearQueue();
         vcrIntegration.renderQueue();
     }
 
+    handleRemoveFromQueueEvent(event) {
+        console.log('remove from queue');
+        const url = $(event.target).parent().attr('data-vcr-url');
+        if (vcrIntegration.removeFromQueue(url)) {
+            vcrIntegration.renderQueue();
+        }
+    }
+
     handleAddToQueueEvent(event) {
         const url = $(event.target).attr('data-vcr-url');
-        if(url) {
-            vcrIntegration.addToQueue(url);
+        const title = $(event.target).attr('data-vcr-title');
+        if (url) {
+            vcrIntegration.addToQueue(url, title);
             // TODO: disable buttons of items that are already in queue
         }
     }
 }
 
-const init_plugin = function() {
+const init_plugin = function () {
     vcrIntegration = new VCRIntegration({});
     const eventHandler = new VCRIntegrationEventHandler(vcrIntegration);
-    
+
     $("body").on("click", "#vcrQueue #clearVcrQueue", eventHandler.handleClearQueueEvent)
-    
+    $("body").on("click", "#vcrQueue li[data-vcr-url] a.removeFromVcrQueue", eventHandler.handleRemoveFromQueueEvent)
+
     // if auto registration of handlers enabled
-    if($("a[data-vcr-url]").length) {
+    if ($("a[data-vcr-url]").length) {
         console.log("Found one or more VCR queue item controls: " + $("a[data-vcr-url]").length);
-        
+
         // TODO: render 'add to queue' buttons where placeholders are defined
-        
+
         // bind event to add to queue
         $("body").on("click", "a[data-vcr-url]", eventHandler.handleAddToQueueEvent);
     }
-    
+
     const queue = vcrIntegration.getQueue();
-    if(queue) {
+    if (queue) {
         vcrIntegration.renderQueue();
     }
 };
