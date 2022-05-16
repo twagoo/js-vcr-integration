8588d86d3b101b01ea36e1b1bb659d00b53c6230
Twan Goosen <twan@clarin.eu>
2022-05-16 11:28:42 +0200

    VCR plugin: webpack + babel

---
+++ vlo-web-app/src/main/webapp/assets/vcr-plugin/webpack.config.js
@@ -0,0 +1,24 @@
+var path = require('path');
+
+module.exports = {
+   entry: {
+      app: './src/main.js'
+   },
+   output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'vcr-integration.js'
+   },
+   mode:'development',
+   module: {
+      rules: [
+         {
+            test: /\.js$/,
+            include: path.resolve(__dirname, 'src'),
+            loader: 'babel-loader',
+            options: {
+               "presets": ["@babel/preset-env"]
+            }
+         }
+      ]
+   }
+};
