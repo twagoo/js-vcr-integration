0848afaf9d8a78bf39687436e6ef7b1daf06327d
Twan Goosen <twan@clarin.eu>
2022-08-30 11:12:31 +0300

    Banner in dist output

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,8 @@
 const path = require('path');
+const webpack = require('webpack');
+const TerserPlugin = require("terser-webpack-plugin");
+
+const pkg = require('./package.json');
 const babelConfig = require('./babel.config.js');
 const handlebarsConfig = require('./handlebars.config.js');
 
@@ -6,6 +10,10 @@ const handlebarsConfig = require('./handlebars.config.js');
  * Webpack BASE configuration
  * 
  */
+
+ const banner = `Virtual Collection Registry integration plugin v${pkg.version}
+ Licensed GPLv3. CLARIN ERIC - https://www.clarin.eu`;
+
 module.exports = {
     mode: 'production',
     entry: {
@@ -35,12 +43,30 @@ module.exports = {
                 test: /\.(css|scss)$/i,
                 include: path.resolve(__dirname, 'src/style'),
                 use: [
-                    {loader: "style-loader"},
-                    {loader: "css-loader"},
-                    {loader: 'sass-loader', options: {sourceMap: true}},
-                    {loader: 'encapsulated-css-loader', options: {className: 'vcr-plugin'}}
+                    { loader: "style-loader" },
+                    { loader: "css-loader" },
+                    { loader: 'sass-loader', options: { sourceMap: true } },
+                    { loader: 'encapsulated-css-loader', options: { className: 'vcr-plugin' } }
                 ],
             }
         ]
-    }
+    },
+    optimization: {
+        //minimize: production,
+        minimizer: [
+            new TerserPlugin({
+                parallel: require('os').cpus().length,
+                terserOptions: {
+                    ie8: false,
+                    keep_fnames: false,
+                    output: {
+                        beautify: false,
+                        comments: (node, { value, type }) =>
+                            type == 'comment2' && value.startsWith('!'),
+                    },
+                },
+            }),
+        ],
+    },
+    plugins: [new webpack.BannerPlugin({ banner })]
 };
